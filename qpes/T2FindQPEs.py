86434eb828feba1b741a7684338ebf8361baab83
emory <emorymurff@macbookpro.home>
2025-11-17 14:01:15 +0100

    base code

---
+++ qpes/T2FindQPEs.py
@@ -0,0 +1,23 @@
+from typing import Sequence, Literal
+
+import pandas as pd
+
+from ampel.abstract.AbsTiedLightCurveT2Unit import AbsTiedLightCurveT2Unit
+from ampel.struct.UnitResult import UnitResult
+from ampel.types import UBson
+from ampel.view.LightCurve import LightCurve
+from ampel.view.T2DocView import T2DocView
+from ampel.model.StateT2Dependency import StateT2Dependency
+
+
+from timewise.process import keys
+
+
+class T2FindQPEs(AbsTiedLightCurveT2Unit):
+    t2_dependency: Sequence[StateT2Dependency[Literal["T2StackVisits"]]]
+
+    def process(self, light_curve: LightCurve, t2_views: Sequence[T2DocView]) -> UBson | UnitResult:
+        records = [r.body[0] for r in t2_views][0]
+        stacked_lightcurve = pd.DataFrame.from_records(records)
+        print(stacked_lightcurve)
+
